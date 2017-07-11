af9005fc46dfdcfedde35410a7fddb1373a621dd
wombach <andreas.wombacher@gmail.com>
2017-07-11 12:31:07 +0200

    first time a form works with the FormGroup
    the magic is to make each form an individual component and to call it
    the right way from the base.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ng2-cookies';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  providers: [ CookieService ],
+  template: '<app-hero-form></app-hero-form>',
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
+  cookies: Object;
+  user: string;
+  keys: Array<string>;
+  cName: string;
+  cValue: string;
+  rName: string;
+  checkName: string;
+
+  constructor( public cookieService: CookieService ) {
+    this.update();
+    /* this is for testing only */
+    this.addCookie('wordpress_logged_in_6ebf9f7ef5b8a2a1a06c62cc50693637',
+'Andreas+Wombacher|1499936566|6PS6n0a3lboEJpCJ3ZXIBcpJLJxOJRX2Isel8Uizq6g|' +
+'7fd7d94a03aa6ddaca389fa0f7dbf90f976a8730936ebaf07efa83b8d258254a');
+    console.log(this.cookies);
+  }
+  update() {
+    this.cookies = this.cookieService.getAll();
+    this.keys = Object.keys(this.cookies);
+    /*for (let key of this.keys) {
+      if (key =)
+    }*/
+  }
+  addCookie(cName: string, cValue: string) {
+    console.log('Adding: ', cName, cValue);
+    this.cookieService.set(cName, cValue);
+    this.update();
+  }
+  removeCookie(rName: string) {
+    console.log('Removing: ', rName);
+    this.cookieService.delete(rName);
+    this.update();
+  }
+  removeAll() {
+    console.log('Removing all cookies');
+    this.cookieService.deleteAll();
+    this.update();
+  }
+  checkCookie() {
+    console.log('Checking: ', this.checkName);
+    console.log(this.cookieService.check(this.checkName));
+    window.alert('Check cookie ' + this.checkName + ' returned ' + this.cookieService.check(this.checkName));
+  }
 }
---
+++ src/app/hero.ts
@@ -0,0 +1,13 @@
+/**
+ * New typescript file
+ */
+export class Hero {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {  }
+
+}
